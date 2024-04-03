1ddcbd9225b4738c3b69ac91de4cd8530836a3b1
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-04-02 23:25:18 -0300

    ok

--- src/ListaDeExercicios/repeticao_4/Program.cs
+++ src/ListaDeExercicios/repeticao_4/Program.cs
@@ -1,7 +1,11 @@
 ﻿
 for (int i = 1; i < 100; i++)
 {
-    if (i % 3 == 0)
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz, ");
+    } else if (i % 3 == 0)
     {
         Console.WriteLine("Fizz, ");
     } else if (i % 5 == 0) 
